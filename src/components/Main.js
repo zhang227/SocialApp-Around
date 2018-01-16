2595c8a25528928efdc6f83426ae239738f11ceb
zhang227 <zhang.jinf@husky.neu.edu>
2018-01-15 22:08:06 -0600

    router ready

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {Login} from './Login'
+import { Switch, Route } from 'react-router'
 import { WrappedRegister } from './Register';
 
 export class Main extends React.Component {
     render() {
         return (
-            <section className="main">
-                <Login/>
-            </section>
+            <div className="main">
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/Register" component={WrappedRegister}/>
+                    <Route path="/Login" component={Login}/>
+                    <Route component={Login}/>
+                </Switch>
+            </div>
         );
     }
 }
